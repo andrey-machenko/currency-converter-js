73f79e4ec2064dd7c4e1e0bba3ff7878cfbd20e1
= <=>
2021-10-07 18:30:21 +0300

    New layout and changes in the project logic

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 const rates = {};
-const elementBYN = document.querySelector('[data-value="BYN"]')
-const elementUSD = document.querySelector('[data-value="USD"]')
+const elementBYN = document.getElementById("BYN");
+const elementUSD = document.getElementById("USD");
 
-const input = document.querySelector('#input');
-const result = document.querySelector('#result');
-const select = document.querySelector('#select');
+const input = document.getElementById("input");
+const result = document.getElementById("result");
+const select = document.getElementById("select");
+
 
 getCurrency();
 
-async function getCurrency () {
+async function getCurrency() {
     const answer = await fetch('https://www.nbrb.by/api/exrates/rates?periodicity=0');
     const data = await answer.json();
-    rates.USD = data.find( currency => currency.Cur_Abbreviation === "USD")
-}
-
-input.oninput = function() {
-    const rate = (parseFloat(input.value) / rates.USD.Cur_OfficialRate).toFixed(2);
-    result.value = rate;
+    rates.USD = data.find(currency => currency.Cur_Abbreviation === "USD");
     elementUSD.textContent = rates.USD.Cur_OfficialRate;
     elementBYN.textContent = (1 / rates.USD.Cur_OfficialRate).toFixed(2);
 }
 
-result.oninput = function() {
-    const rate = (parseFloat(result.value) * rates.USD.Cur_OfficialRate).toFixed(1);
+input.oninput = function () {
+    if (input.value > 0) {
+        const rate = (parseFloat(input.value) / rates.USD.Cur_OfficialRate).toFixed(2);
+        result.value = rate;
+    } else {
+        result.value = "";
+    }
+}
+
+result.oninput = function () {
+    const rate = (parseFloat(result.value) * rates.USD.Cur_OfficialRate).toFixed(2);
     input.value = rate;
-    elementBYN.textContent = (1 / rates.USD.Cur_OfficialRate).toFixed(2);
-    elementUSD.textContent = rates.USD.Cur_OfficialRate;
 }
-
-
-
